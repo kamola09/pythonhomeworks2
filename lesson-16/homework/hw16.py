4ba6ec9a38f186372aa5dc878c62431c53d2085c
kamola09 <kamolboyevakamola01@gmail.com>
2025-10-30 13:29:52 +0500

    Create hw16.py

---
+++ lesson-16/homework/hw16.py
@@ -0,0 +1,54 @@
+Dasturlaw, [30.10.2025 11:52]
+import numpy as np
+data=np.array([12.23, 13.32, 100, 36.32])
+print(data)
+
+Dasturlaw, [30.10.2025 12:04]
+matrix=np.array([[ 2,3,4], 
+                 [ 5, 6, 7], 
+                 [ 8, 9, 10]])
+print(matrix)
+
+Dasturlaw, [30.10.2025 12:54]
+vector=np.zeros(10)
+vector[5]=11
+print(vector)
+
+Dasturlaw, [30.10.2025 12:55]
+random1=np.arange(12,38,1)
+print(random1)
+
+Dasturlaw, [30.10.2025 13:00]
+array_number=np.array([1,2,3],dtype=float)
+print(array_number)
+
+celsius=np.array([0, 12, 45.21, 34, 99.91])
+others=np.array([-17.78, -11.11, 7.34, 1.11, 37.73, 0. ])
+fanherit=np.append(celsius,32)
+print(fanherit)
+
+
+print(others)
+
+
+grade=np.array([10, 20, 30])
+grade1=np.append(grade,[40,50,60,70,80,90])
+print(grade1)
+
+
+number=np.arange(1,10)
+avg=np.mean(number)
+median1=np.median(number)
+print(avg)
+print(median1)
+
+
+number=np.random.random((10,10))
+max1=np.max(number)
+min1=np.min(number)
+print(max1)
+print(min1)
+
+
+number=np.random.random((3,3,3))
+print(number)
